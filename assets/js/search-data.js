fac42f3809ec382ae133c78c9383800654510d9c
majidmanzoor170 <165884581+majidmanzoor170@users.noreply.github.com>
2025-11-18 08:16:37 +0000

    Deploying to gh-pages from @ majidmanzoor170/website.github.io@a9b4362b5f11f0dace59d862124337df23480400 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,14 +508,35 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%61%6A%69%64%6D%61%6E%7A%6F%6F%72%31%37%30@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/majidmanzoor170", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/manzoormajid", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Majid-Manzoor-2/", "_blank");
         },
       },{
         id: 'social-rss',
